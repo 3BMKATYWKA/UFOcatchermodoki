a39103a27a0d8e63cca80bf52aaa46bb44ef81f1
Sasasan <u431and@yahoo.co.jp>
2020-08-14 00:36:18 +0900

    追加　Goods生成
    ゲーム開始時にGoodsオブジェクトを生成する

---
+++ Assets/GoodsFactoryScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodsFactoryScript : MonoBehaviour
+{
+    public GameObject Goods;
+    private int numGoods;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numGoods = 10;
+        float y = 15f;
+
+        for (int x=-1; x<6; x+=4)
+        {
+            for(int z=-7; z<10; z+=4)
+            {
+                Instantiate(Goods, new Vector3(x, y, z), Quaternion.identity);
+            }         
+        }
+
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
